2213f1bc7e08e00569be63f04827d7271d9548b4
juanortiz10 <juanjo.jjo94@gmail.com>
2015-06-09 23:55:04 -0500

    DataBaseCreation

---
+++ app/src/main/java/com/example/juan/eduquer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.juan.eduquer;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.widget.ListView;
+
+
+
+public class MainActivity extends ActionBarActivity {
+    private String[] things;
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}

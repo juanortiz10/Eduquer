c576903bde6006786d8d68cbb27ce02a6e6259ad
juanortiz10 <juanjo.jjo94@gmail.com>
2015-06-20 16:41:24 -0500

    Searcher Working Correctly

--- app/src/main/java/fragments/Look.java
+++ app/src/main/java/fragments/Look.java
@@ -6,52 +6,47 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.juan.eduquer.R;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
-import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
-
-import adapter.TutorialsInfoAdapter;
+import java.util.ArrayList;
+import adapter.SearchAdapter;
+import helper.DataBaseHelper;
+import models.Item;
+import models.Items;
 import models.Result;
 import models.Search;
 
 /**
  * Created by juan on 10/06/15.
  */
-public class Look extends Fragment implements OnClickListener, OnItemClickListener{
+public class Look extends Fragment implements OnItemClickListener{
 
     private final Search search = new Search();
-    private ImageButton ibDoSearch;
-    private TextView tvSearch;
-    private ListView lvResults;
+    private ListView listViewResults;
     private Result result = new Result();
-
+    private ArrayList<Items> results=new ArrayList();
+    private ArrayList<Item> words;
+    private SearchAdapter searchAdapter;
     public Look(){}
 
-    final Handler handler = new Handler();
-    final Runnable updateItems = new Runnable(){
+    final Handler handlerTask = new Handler();
+    final Runnable updateItemsTask = new Runnable(){
         public void run(){
-            updateItemsInUI();
+            updateItems();
         }
     };
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedIntanceState){
         View rootView = inflater.inflate(R.layout.look,container,false);
-        lvResults = (ListView)  rootView.findViewById(R.id.lvResults);
-        lvResults.setOnItemClickListener(this);
-        tvSearch = (TextView) rootView.findViewById(R.id.tvSeach);
-        ibDoSearch = (ImageButton)rootView.findViewById(R.id.ibSearch);
-        ibDoSearch.setOnClickListener((android.view.View.OnClickListener) this);
+        listViewResults = (ListView)  rootView.findViewById(R.id.lvResults);
+        listViewResults.setOnItemClickListener(this);
+        search();
+        searchAdapter=new SearchAdapter(getActivity().getApplicationContext(),results);
         return rootView;
     }
 
@@ -62,51 +57,44 @@ public class Look extends Fragment implements OnClickListener, OnItemClickListen
         } catch (Exception e) {
             e.printStackTrace();
         }
+        results.addAll(newResult.getItems());
         return newResult;
     }
 
-    protected void updateItemsInUI() {
+    protected void updateItems() {
         if(0 == result.getItems().size()){
-            Toast.makeText(getActivity().getApplicationContext(), "No se ha encontrado ningún resultado", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity().getApplicationContext(), "We've had some problems please reload the app", Toast.LENGTH_SHORT).show();
         }
-        TutorialsInfoAdapter tutorialsInfoAdapter = new TutorialsInfoAdapter(getActivity().getApplicationContext(), result.getItems());
-        lvResults.setAdapter(tutorialsInfoAdapter);
+        listViewResults.setAdapter(searchAdapter);
+        searchAdapter.notifyDataSetChanged();
     }
 
+    private void search(){
+        DataBaseHelper dataBaseHelper = new DataBaseHelper(getActivity().getApplicationContext());
+        this.words = new ArrayList();
+        this.words = dataBaseHelper.getALl();
+        if (words.size() > 0) {
+            result.getItems().clear();
+            int random = (int) (Math.random() * (this.words.size()-1 + 1) + 0);
+            final String textToSearch = this.words.get(random).getName();
+            Thread t = new Thread() {
+                public void run() {
+                    result = updateResult(textToSearch);
+                    handlerTask.post(updateItemsTask);
+                }
+            };
 
-
-
-
-    @Override
-    public void onClick(View arg0) {
-        switch(arg0.getId())
-        {
-            case R.id.ibSearch :
-                final String textToSearch = tvSearch.getText().toString().trim();
-                Thread t = new Thread(){
-                    public void run(){
-                        result = updateResult(textToSearch);
-                        handler.post(updateItems);
-                    }
-                };
-                Toast.makeText(getActivity().getApplicationContext(),"Realizando búsqueda",Toast.LENGTH_SHORT).show();
-                t.start();
-                hideInputMethod();
-                break;
+            Toast.makeText(getActivity().getApplicationContext(), "Searching... " + textToSearch, Toast.LENGTH_SHORT).show();
+            t.start();
+        }else{
+            Toast.makeText(getActivity().getApplicationContext(), "Your word's list is empty",Toast.LENGTH_SHORT).show();
         }
-    }
-
-    private void hideInputMethod(){
-        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(tvSearch.getWindowToken(), 0);
     }
 
     @Override
     public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getItems().get(arg2).getLink()));
         startActivity(intent);
     }
-
 }
 

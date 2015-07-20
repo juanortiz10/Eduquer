9738d7b5e2585a4b481291bc81cb56ba33ec33e8
juanortiz10 <juanjo.jjo94@gmail.com>
2015-07-20 00:02:21 -0500

    Fixes #23

--- app/src/main/java/fragments/Look.java
+++ app/src/main/java/fragments/Look.java
@@ -2,60 +2,67 @@ package fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.juan.eduquer.R;
-import com.example.juan.eduquer.Webview;
-import android.content.Intent;
 import android.os.Handler;
-import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-import adapter.SearchAdapter;
 import algorithms.CheckProgress;
 import helper.DataBaseHelper;
 import models.Item;
 import models.Items;
 import models.Result;
 import models.Search;
+import recycler.RVAdapter;
 
 /**
  * Created by juan on 10/06/15.
  */
-public class Look extends Fragment implements OnItemClickListener{
+public class Look extends Fragment implements{
     public int seconds=0;
     private final Search search = new Search();
-    private ListView listViewResults;
+ //   private ListView listViewResults;
     private Result result = new Result();
     private ArrayList<Items> results=new ArrayList();
     private ArrayList<Item> words;
-    private SearchAdapter searchAdapter;
+//    private SearchAdapter searchAdapter;
     public String textToSearch;
     private ImageView imvNo;
     private TextView tvNo;
     Timer timer=new Timer();
+    RecyclerView rv;
+    List<Items> items=new ArrayList<>();
+
     public Look(){}
 
     final Handler handlerTask = new Handler();
+
     final Runnable updateItemsTask = new Runnable(){
         public void run(){
             updateItems();
         }
     };
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedIntanceState){
-        View rootView = inflater.inflate(R.layout.look,container,false);
+        View rootView = inflater.inflate(R.layout.recycler,container,false);
         setHasOptionsMenu(true);
-        listViewResults = (ListView)  rootView.findViewById(R.id.lvResults);
-        listViewResults.setOnItemClickListener(this);
+
+        rv=(RecyclerView)rootView.findViewById(R.id.rv);
+        rv.setHasFixedSize(true);
+        LinearLayoutManager llm=new LinearLayoutManager(getActivity().getApplicationContext());
+        rv.setLayoutManager(llm);
+
         DataBaseHelper dataBaseHelper=new DataBaseHelper(getActivity().getApplicationContext());
         if(dataBaseHelper.getALl().size()>0) {
             search();
@@ -65,7 +72,6 @@ public class Look extends Fragment implements OnItemClickListener{
             tvNo=(TextView)rootView.findViewById(R.id.tvNo);
             tvNo.setText(getResources().getString(R.string.emptylist));
         }
-        searchAdapter=new SearchAdapter(getActivity().getApplicationContext(),results);
         return rootView;
     }
 
@@ -109,6 +115,7 @@ public class Look extends Fragment implements OnItemClickListener{
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         results.addAll(newResult.getItems());
         return newResult;
     }
@@ -117,8 +124,8 @@ public class Look extends Fragment implements OnItemClickListener{
         if(0 == result.getItems().size()){
             Toast.makeText(getActivity().getApplicationContext(),getResources().getString(R.string.problems), Toast.LENGTH_SHORT).show();
         }
-        listViewResults.setAdapter(searchAdapter);
-        searchAdapter.notifyDataSetChanged();
+        RVAdapter adapter=new RVAdapter(results);
+        rv.setAdapter(adapter);
     }
 
     private void search(){
@@ -143,14 +150,13 @@ public class Look extends Fragment implements OnItemClickListener{
         }
         dataBaseHelper.close();
     }
-
+/*
     @Override
     public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
         Intent intent=new Intent(getActivity(),Webview.class);
         intent.putExtra("link", (result.getItems().get(arg2).getLink()));
         startActivityForResult(intent,1);
-    }
-
+    }*/
     class Time extends TimerTask {
         @Override
         public void run() {
